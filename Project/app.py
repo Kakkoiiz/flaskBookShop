cdfe1f5c0d0d5031b68e452bb79673b77af71871
Syn <ngmanhdung2003@gmail.com>
2023-05-03 22:04:29 +0700

    create crud

--- Project/app.py
+++ Project/app.py
@@ -15,16 +15,86 @@ def home():
 
 @app.route('/crud')
 def crud():
-   title = request.args.get('title')
    
+   
+   title = request.args.get('title')
    if title != None:
       cursor.execute(
-         f'SELECT * FROM books WHERE title LIKE "%{title}%"')
+         f'SELECT * FROM books WHERE Title LIKE "%{title}%"')
    else:
       cursor.execute('SELECT * FROM books')
    books = cursor.fetchall()
    return render_template('crud.html', books = books, len = len(books), base64 = base64)
 
+
+# Tạo đường dẫn add book
+@app.route('/add', methods = ['POST'])
+def add():
+   # Lấy các giá trị ở trong form qua phương thức get
+   if request.method == 'POST':
+      book_id = request.form.get('book_id')
+      title = request.form.get('title')
+      author = request.form.get('author')
+      genre = request.form.get('genre')
+      year = request.form.get('year')
+      price = request.form.get('price')
+      img = request.files.get('img')
+      # Xử lý hình ảnh
+      img_binary = None
+      if img:
+         img_file = request.files['img']
+         img_data = img_file.read()
+         img_pil = Image.open(io.BytesIO(img_data))
+         img_buffer = io.BytesIO()
+         img_pil.save(img_buffer, format='JPEG')
+         img_binary = img_buffer.getvalue()
+         img_base64 = base64.b64encode(img_data)
+       # Thêm thông tin book vào cơ sở dữ liệu
+      sql = f'INSERT INTO books (BookID, Title, AuthorName, GenreName, PublishYear, Price, Image) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+      val = (book_id, title, author, genre, year, price, img_binary)
+      cursor.execute(sql, val)
+      mydb.commit()
+      # trả về đường dẫn crud
+   return redirect('/crud')
+
+
+@app.route('/update', methods=['GET', 'POST'] )
+def update():
+   # Lấy các giá trị ở trong form qua phương thức get
+   if request.method == 'POST':
+      book_id = request.form.get('book_id')
+      title = request.form.get('title')
+      author = request.form.get('author')
+      genre = request.form.get('genre')
+      year = request.form.get('year')
+      price = request.form.get('price')
+      img = request.files.get('img')
+      # Xử lý hình ảnh
+      img_binary = None
+      if img:
+         img_file = request.files['img']
+         img_data = img_file.read()
+         img_pil = Image.open(io.BytesIO(img_data))
+         img_buffer = io.BytesIO()
+         img_pil.save(img_buffer, format='JPEG')
+         img_binary = img_buffer.getvalue()
+         img_base64 = base64.b64encode(img_data)
+       # Thêm thông tin book vào cơ sở dữ liệu
+      sql = 'UPDATE books SET title = %s, AuthorName = %s, GenreName = %s, PublishYear = %s, Price = %s, Image = %s WHERE BookID = %s'
+      val = (title, author, genre, year, price, img_binary, book_id)
+      cursor.execute(sql, val)
+      mydb.commit()
+      # trả về đường dẫn crud
+   return redirect('/crud')
+
+
+@app.route('/delete/<book_id>')
+def delete(book_id):
+   sql = f'DELETE FROM books WHERE BookID = {book_id}'
+   cursor.execute(sql)
+   mydb.commit()
+   return redirect('/crud')
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Project/static/js/app.js
+++ Project/static/js/app.js
@@ -1,28 +1,83 @@
 const avatarUpload = document.querySelector('#avatar-upload');
 const avatarPreview = document.querySelector('#avatar-preview');
-var addBook = document.getElementById('addBook')
+const avatarUpdate = document.querySelector('#avatar_update');
+const avatarUpdatePreview = document.querySelector('#avatar-update-preview');
+var addBook = document.getElementById('add_book');
 var showAddBook = document.getElementById('btn_add');
 var closeAddBook = document.getElementById('close_add')
+var updateBook = document.getElementById('update_book');
+var showUpdateBook = document.querySelectorAll('.js_update_book');
+var closeUpdateBook = document.querySelectorAll('.js_close_update');
+
+
+avatarUpload.addEventListener('change', function() {
+  const reader = new FileReader();
+  
+  reader.addEventListener('load', function(){
+    avatarPreview.setAttribute('src', reader.result);
+  });
+  
+  reader.readAsDataURL(this.files[0]);
+});
+
+
+avatarUpdate.addEventListener('change', function() {
+  const reader = new FileReader();
+
+  reader.addEventListener('load', function(){
+    avatarUpdatePreview.setAttribute('src', reader.result);
+  });
+
+  reader.readAsDataURL(this.files[0]);
+});
+
 
 function showAdd () {
-    if (addBook.style.display == 'block') {
-      addBook.style.display = 'none'
-    } else {
-      addBook.style.display = 'block'
-    }
+  if (addBook.style.display == 'block') {
+    addBook.style.display = 'none'
+  } else {
+    addBook.style.display = 'block'
+  }
 }
-
 showAddBook.addEventListener('click', showAdd)
 closeAddBook.addEventListener('click', showAdd)
 
-avatarUpload.addEventListener('change', function() {
-    const reader = new FileReader();
+
+function showUpdate() {
+  if (updateBook.style.display == 'block') {
+      updateBook.style.display = 'none'
+  } else {
+      updateBook.style.display = 'block'
+  }
+}
+
+for (var i = 0; i < showUpdateBook.length; i++){
+  showUpdateBook[i].addEventListener('click', showUpdate)
+}
+
+for (var i = 0; i < closeUpdateBook.length; i++){
+  closeUpdateBook[i].addEventListener('click', showUpdate)
+}
+
+
+function onUpdateBook(book_string) {
+  book_string = book_string.replaceAll("'", "").replaceAll('(', '').replaceAll(')', '').trim();
+  var book = book_string.split(",");
+  var u_book_id = document.getElementById("u_book_id");
+  var u_title = document.getElementById("u_title");
+  var u_author = document.getElementById("u_author");
+  var u_genre = document.getElementById("u_genre");
+  var u_year = document.getElementById("u_year");
+  var u_price = document.getElementById("u_price");
+  var avatar_update = document.getElementById("avatar_update");
+
+  u_book_id.value = book[0].trim();
+  u_title.value = book[1].trim();
+  u_author.value = book[2].trim();
+  u_genre.value = book[3].trim();
+  u_year.value = book[4].trim();
+  u_price.value = book[5].trim();
+  avatar_update = book[6].trim();
   
-    reader.addEventListener('load', function(){
-      avatarPreview.setAttribute('src', reader.result);
-    });
-  
-    reader.readAsDataURL(this.files[0]);
-  });
-  
-  
+}
+
